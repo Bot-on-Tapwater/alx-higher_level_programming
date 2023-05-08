d6672ed1db747b8cd632bf5aef52229887d6cc36
Bot_on_Tapwater <mundabrandon@outlook.com>
2023-05-08 21:17:43 +0300

    Copy - Cut - Paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+print(f"First 3 letters: {word[:3]:s}")
+print(f"Last 2 letters: {word[-2:]:s}")
+print(f"Middle word: {word[1:-1]}")
