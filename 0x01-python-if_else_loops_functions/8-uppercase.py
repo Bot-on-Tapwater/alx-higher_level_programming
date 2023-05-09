ca2c1f08b84a4cc85250cd0435ae826e7711e1ec
Bot_on_Tapwater <mundabrandon@outlook.com>
2023-05-09 21:26:20 +0300

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def uppercase(str):
+    modified_string = ""
+    for c in str:
+        if (ord(c) >= 97 and ord(c) <= 122):
+            modified_char = chr(ord(c) - 32)
+            modified_string += modified_char
+        else:
+            modified_string += c
+    print("{}".format(modified_string))
