cedb89a08d9f6a30c827fbfa9452541a5c92d2d5
Bot_on_Tapwater <mundabrandon@outlook.com>
2023-05-09 20:37:23 +0300

    Implement ord()

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def islower(c):
+    # if (ord(c) >= 97 and ord(c) <= 122):
+    #     return True
+    # else:
+    #     return False
     for i in range(97, 123):
-        if (c == chr(i)):
+        if (ord(c) == i):
             return True
     return False
