013250ec7630921d5b25c00b122aa65376f87402
Bot_on_Tapwater <mundabrandon@outlook.com>
2023-05-11 15:57:15 +0300

    Use from ## import ##

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import variable_load_5
+from variable_load_5 import a
+# import variable_load_5
 
 if (__name__ == '__main__'):
-    print("{:d}".format(variable_load_5.a))
+    print("{:d}".format(a))
