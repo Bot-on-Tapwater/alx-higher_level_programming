214982385746338e13389742ab69b514ad44cd33
Bot_on_Tapwater <mundabrandon@outlook.com>
2023-05-11 15:23:52 +0300

    Alternative infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 import sys
-counter = 0
-sum = 0
 
 if __name__ == '__main__':
-    for args in sys.argv:
-        if (len(sys.argv) == 1):
-            print("{:d}".format(sum))
-        else:
-            if (counter != 0):
-                sum += int(args)
-            counter += 1
-    print("{:d}".format(sum))
+    arguments = sys.argv[1:]
+
+    result = sum([int(arg) for arg in arguments])
+
+    print("{:d}".format(result))
