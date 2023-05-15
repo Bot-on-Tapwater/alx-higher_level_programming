59a5b79ec548f2802265ec2d7f5f351f24bcbd17
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-05-15 12:01:43 +0300

    Refactor code using reversed()

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for integer in range(-1, -len(my_list) - 1, -1):
-        print("{:d}".format(my_list[integer]))
+    for integer in reversed(my_list):
+        print("{:d}".format(integer))
