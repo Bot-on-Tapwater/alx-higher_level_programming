44502c278bada791dd7faff51bfb9d3afe19934e
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-05-15 12:19:11 +0300

    Replace in a copy

--- 0x03-python-data_structures/throwaway.py
+++ 0x03-python-data_structures/throwaway.py
@@ -4,3 +4,10 @@
 def print_reversed_list_integer(my_list=[]):
     for integer in range(-1, -len(my_list) - 1, -1):
         print("{:d}".format(my_list[integer]))
+
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    for integer in reversed(my_list):
+        print("{:d}".format(integer))
