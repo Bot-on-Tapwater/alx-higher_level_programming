fd9bd3bebbc69e32119809df297c81e0a166063e
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-05-15 13:35:38 +0300

    Find the max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    max_int = 0
+
+    for integer in my_list:
+        if (max_int < integer):
+            max_int = integer
+        else:
+            continue
+
+    return max_int
