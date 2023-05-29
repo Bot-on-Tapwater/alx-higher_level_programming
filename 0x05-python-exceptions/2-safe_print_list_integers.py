9c78ea3a3768a8028922cbeb975b69503d1d5890
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-05-29 13:56:17 +0300

    Undo changes

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(x):
-            try:
-                if isinstance(my_list[i], int):
-                    print("{:d}".format(my_list[i]), end="")
-                    count += 1
-            except IndexError:
-                break
-    except TypeError:
-        raise Exception("An exception occurred: x is larger than the length of my_list")
-    finally:
-        print()
-        return count
+    count = 0
+
+    for elem in range(x):
+        try:
+            print("{:d}".format(my_list[elem]), end="")
+            count += 1
+ 
+        except (ValueError, TypeError):
+            continue
+
+    print()
+    return (count)
