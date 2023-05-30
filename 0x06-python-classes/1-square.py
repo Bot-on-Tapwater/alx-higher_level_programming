52dbe3ec96456f0d4309464c3acb3d2be7b0d768
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-05-30 17:33:08 +0300

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+# 0-square.py
+# Munda Brandon <mundabrandon@outlook.com>
+"""Define a class Square with size attribute"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
