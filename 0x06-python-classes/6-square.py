a8d214fd0cd31c955994e82ec9c4fc3d4d64fdb9
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-05-30 19:55:43 +0300

    Check if tuple has 2 integers

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,7 +38,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple)):
+        if (not isinstance(value, tuple) or len(value) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
         elif (not isinstance(value[0], int) or value[0] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
