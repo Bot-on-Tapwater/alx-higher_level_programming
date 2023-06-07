25dd044b8023e084ba371d84f93e8dfd75ce4f31
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-06-07 11:54:37 +0300

    Add more tests

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,20 +5,27 @@ Inserts double newline after "." "?" ":"
 
 
 def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    punctuation = [".", "?", ":"]
-    result = ""
-    line = ""
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
-    for char in text:
-        line += char
-        if char in punctuation:
-            result += line.strip() + "\n\n"
-            line = ""
-
-    if line:
-        result += line.strip()
-
-    print(result, end="")
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
