01e6ca3bcfb5518f409b038c54f33645b829b07a
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-06-13 17:44:52 +0300

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/python3
+"""
+returns dictionary desc for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """
+    returns dictionary desc for JSON serialization of an object
+    """
+
+    return (vars(obj))
