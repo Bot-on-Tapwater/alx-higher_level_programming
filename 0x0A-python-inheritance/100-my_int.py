7c9c2e7dc7f85b7f0d97183605e7be8f03ada1e7
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-06-14 20:58:15 +0300

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+create class MyInt
+"""
+
+
+class MyInt(int):
+    def __eq__(self, other):
+        """magic function"""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """magic function"""
+        return super().__eq__(other)
