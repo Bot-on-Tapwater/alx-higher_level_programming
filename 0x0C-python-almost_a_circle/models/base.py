7d89a77a7ebfa91278a18c14500a275e8eedf8f4
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-06-19 18:47:35 +0300

    More changes on to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,10 +32,9 @@ class Base:
         """
         Convert dictionary to JSON repr
         """
-        if (len(list_dictionaries) == 0 or list_dictionaries is None):
+        if (list_dictionaries == [] or list_dictionaries is None):
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
