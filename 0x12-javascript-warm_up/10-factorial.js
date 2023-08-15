39f17995f6a3fcd6abb5e326a9d5ef94868d8c19
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-08-15 08:47:39 +0300

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const a = process.argv[2];
+
+function convNum (num) {
+  /* console.log('Converting to Integer'); */
+  const number = parseInt(num);
+  return (number);
+}
+
+function factorial (num) {
+  const number = convNum(num);
+
+  if (isNaN(number)) {
+    return (1);
+  } else if (number > 0) {
+    return (number * factorial(number - 1));
+  } else {
+    return (1);
+  }
+}
+
+console.log(`${factorial(a)}`);
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+function factorial(num)
+{
+	if (num > 0)
+	{
+		return (num * factorial((num - 1)));
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+if (Number.isInteger(Number(arg))) {
+	console.log(`${factorial(parseInt(arg))}`);
+      } else {
+	console.log(`${1}`);
+      }
+
