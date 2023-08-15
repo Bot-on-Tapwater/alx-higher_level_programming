e8033762d76f9af0b2080264b8783c0c82c6fd4a
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-08-15 08:17:02 +0300

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const arg = process.argv[2];
+
+if (Number.isInteger(Number(arg))) {
+  /* console.log(`My number: ${parseInt(arg)}`); */
+  const numLoop = parseInt(arg);
+  for (let i = 0; i < numLoop; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
