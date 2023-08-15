99e58991497aa1fd46b578006bc6dd187ee48c5e
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-08-15 07:34:10 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+/* import { argv } from 'node:process'; */
+
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
+}
