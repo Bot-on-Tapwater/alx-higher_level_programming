cbce5392f4ba16ec6bf2f22faabb1785dee578fb
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-08-15 08:06:28 +0300

    Correct An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const arg = process.argv[2];
 
 if (Number.isInteger(Number(arg))) {
-  console.log(`${parseInt(arg)}`);
+  console.log(`My number: ${parseInt(arg)}`);
 } else {
   console.log('Not a number');
 }
