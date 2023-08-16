4e2e9d4f647bdba8a5c0c43067c1247073eab327
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-08-16 09:10:58 +0300

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const ogSquare = require('./5-square');
+
+class Square extends ogSquare {
+  charPrint (c) {
+    /* console.log('We printing in this bitch'); */
+    if (c !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(`${`${c}`.repeat(this.width)}`);
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(`${'X'.repeat(this.width)}`);
+      }
+    }
+  }
+}
+
+module.exports = Square;
