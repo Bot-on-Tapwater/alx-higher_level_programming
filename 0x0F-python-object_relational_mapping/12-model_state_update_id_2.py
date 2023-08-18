3ec72e90d8703604f7b6c8e717672e37ddb67e68
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-08-18 12:27:19 +0300

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,44 @@
 #!/usr/bin/python3
+"""Update state object"""
+
+if __name__ == '__main__':
+
+    import sqlalchemy
+    from sqlalchemy import create_engine,\
+        Column, Integer, UniqueConstraint, String
+    from sqlalchemy.ext.declarative import declarative_base
+    from model_state import Base, State
+    import sys
+    from sqlalchemy.orm import sessionmaker
+
+    USR = sys.argv[1]
+    PWD = sys.argv[2]
+    DB = sys.argv[3]
+    HST = 'localhost'
+    PRT = '3306'
+
+    connection_url = f"mysql://{USR}:{PWD}@{HST}:{PRT}/{DB}"
+
+    """Engine to connect to DB"""
+    engine = create_engine(connection_url)
+
+    """Session class for session objects"""
+    Session = sessionmaker(bind=engine)
+
+    """Session with database started"""
+    session = Session()
+
+    # """New state"""
+    # new_state = State(name='Louisiana')
+
+    # """Add new state"""
+    # session.add(new_state)
+
+    """Query state to update"""
+    states = session.query(State).filter(State.id == '2').first()
+
+    """Update the state"""
+    states.name = 'New Mexico'
+
+    """Commit changes to DB"""
+    session.commit()
