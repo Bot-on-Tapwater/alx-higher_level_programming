65cd5c497856afcab7a4536e4052961d25cc5281
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-08-18 11:52:51 +0300

    Print NoThInG

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     """Sample query"""
     states = session.query(State).order_by(State.id.asc()).first()
 
-    if (states.id != ''):
+    if (states is None):
         print(f"{states.id}: {states.name}")
 
     else:
