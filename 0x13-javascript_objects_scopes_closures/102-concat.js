b9dee55131d15b4de986f3626c5b0de49695ded4
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-08-19 05:48:43 +0300

    Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+if (process.argv.length !== 5) {
+  console.error('Usage: ./102-concat.js <fileA> <fileB> <fileC>');
+}
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+const contentA = fs.readFileSync(fileA, 'utf8');
+const contentB = fs.readFileSync(fileB, 'utf8');
+
+const concatenatedContent = contentA + contentB;
+
+fs.writeFileSync(fileC, concatenatedContent);
