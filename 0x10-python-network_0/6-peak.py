8f35902b1120b9762a6ba9f78c84dcd949cd7eab
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-01 22:05:30 +0300

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Find peak in a list of integers"""
+
+
+def find_peak(list_of_integers):
+    "Finds peak in a list of ints"
+    if (len(list_of_integers) > 0):
+        for index, value in enumerate(list_of_integers):
+            try:
+                lft_int = list_of_integers[(index - 1)]
+            except IndexError:
+                lft_int = 0
+
+            try:
+                rgt_int = list_of_integers[(index + 1)]
+            except IndexError:
+                rgt_int = 0
+
+            if (value >= lft_int and value >= rgt_int):
+                return value
+
+    else:
+        return None
