5ff3b0fba82e7bac375a53cf301bb60f320431ec
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-04 10:17:49 +0300

    Error code#1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Handle errors in Requests"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+
+    with requests.get(url) as response:
+        if response.status_code == 200:
+            print(response.text)
+
+        elif response.status_code >= 400:
+            print(f"Error code: {response.status_code}")
