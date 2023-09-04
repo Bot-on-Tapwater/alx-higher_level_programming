51aa767e82b7937661e3ce11f60a2f5fce12c622
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-04 11:08:16 +0300

    My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Get github id"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    username = sys.argv[1]
+    access_token = sys.argv[2]
+
+    url = f"https://api.github.com/users/{username}"
+
+    headers = {"Authorization": f"token {access_token}"}
+
+    with requests.get(url, headers=headers) as response:
+        user_data = response.json()
+        print(user_data.get("id"))
