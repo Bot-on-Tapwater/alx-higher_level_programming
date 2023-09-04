ccd9045752fb43e126801245208219928648f77b
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-04 08:27:13 +0300

    What's my status?#0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Fetch from a host"""
+
+if __name__ == '__main__':
+
+    import urllib.request
+
+    # url to fetch
+    url = "https://alx-intranet.hbtn.io/status"
+
+    # response from url
+    response = urllib.request.urlopen(url)
+
+    # read content of response
+    content = response.read()
+
+    # decode content to a string
+    decoded_content = content.decode("utf-8")
+
+    # print the content
+    print(f"Body response:\n\t- type: {type(content)}\n\t- content: {content}\n\t- utf8 content: {decoded_content}")
+
+    # close the response
+    response.close()
