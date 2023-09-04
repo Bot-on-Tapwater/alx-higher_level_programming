35fd496a1e249942cb083de30dd2cea340b27e0c
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-04 09:23:52 +0300

    Correct pycodestyle errors

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,18 +2,18 @@
 """Sends post request with parameters"""
 
 if __name__ == '__main__':
-        import urllib.request as request
-        import urllib.parse as parse
-        import sys
+    import urllib.request as request
+    import urllib.parse as parse
+    import sys
 
-        url = sys.argv[1]
-        email = sys.argv[2]
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-        variables = {'email':email}
+    variables = {'email': email}
 
-        encoded_data = parse.urlencode(variables).encode('utf-8')
+    encoded_data = parse.urlencode(variables).encode('utf-8')
 
-        rqst = request.Request(url, data=encoded_data, method='POST')
+    rqst = request.Request(url, data=encoded_data, method='POST')
 
-        with request.urlopen(rqst) as response:
-                print(response.read().decode("utf-8"))
+    with request.urlopen(rqst) as response:
+        print(response.read().decode("utf-8"))
