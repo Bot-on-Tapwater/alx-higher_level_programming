515fffe279673aabb0597fe420c7ae70dee31d56
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-04 09:43:12 +0300

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Use requests to fetch url"""
+
+if __name__ == '__main__':
+    import requests
+
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with requests.get(url) as response:
+        # read content of response
+        content = response.text
+
+        # print the content
+        print(f"Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
