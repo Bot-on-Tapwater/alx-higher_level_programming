440e19914fd15ad4949001d3b1a1f544780057ab
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-04 09:50:28 +0300

    Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Use requests to fetch url"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+
+    with requests.get(url) as response:
+        # read content of response
+        content = response.headers['X-Request-Id']
+
+        # print the content
+        print(content)
