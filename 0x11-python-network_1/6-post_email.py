fdf30bda4d67046a1f26e4457e27fb8a627ca21d
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-04 10:04:56 +0300

    POST an email#1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Use requests to send a POST request"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    variables = {'email': email}
+
+    with requests.post(url, data=variables) as response:
+        # read content of response
+        content = response.text
+
+        # print the content
+        print(content)
