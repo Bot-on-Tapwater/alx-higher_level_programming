d340943499637f5b253518838dd34ea9ddab3382
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-04 09:37:07 +0300

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Manage exceptions"""
+
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.error
+    import sys
+
+    # url to fetch
+    url = sys.argv[1]
+
+    # response from url
+    try:
+        with urllib.request.urlopen(url) as response:
+
+            # get response headers
+            print(response.read().decode("utf-8"))
+
+    except urllib.error.URLError as e:
+        print(f"Error code: {e.code}")
