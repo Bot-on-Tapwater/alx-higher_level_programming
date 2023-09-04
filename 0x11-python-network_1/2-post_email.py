6b4370d2a25ffbd82baea4ff17e0f580044fa791
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-04 09:22:40 +0300

    POST an email#0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Sends post request with parameters"""
+
+if __name__ == '__main__':
+        import urllib.request as request
+        import urllib.parse as parse
+        import sys
+
+        url = sys.argv[1]
+        email = sys.argv[2]
+
+        variables = {'email':email}
+
+        encoded_data = parse.urlencode(variables).encode('utf-8')
+
+        rqst = request.Request(url, data=encoded_data, method='POST')
+
+        with request.urlopen(rqst) as response:
+                print(response.read().decode("utf-8"))
