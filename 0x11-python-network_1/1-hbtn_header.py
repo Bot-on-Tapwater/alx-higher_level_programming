c5fe89580fcded1fa50eca5fc9ab4ab7ed4a24ae
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-04 08:56:31 +0300

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Display value of X-Request-Id"""
+
+if __name__ == '__main__':
+    import urllib.request
+    import sys
+
+    # url to fetch
+    url = sys.argv[1]
+
+    print(url)
+
+    # response from url
+    with urllib.request.urlopen(url) as response:
+
+        # get response headers
+        response_headers = dict(response.headers.items())
+
+        # print headers
+        print(response_headers.get("X-Request-Id"))
