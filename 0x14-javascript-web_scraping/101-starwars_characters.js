c5884232f92c47f21fc571de3ec8b8bae672f2b2
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-27 16:43:09 +0300

    Refactor Right Order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,20 @@
 #!/usr/bin/node
-
-const movieId = process.argv[2];
-
-/* url to API */
-const urlReq = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
-
 const request = require('request');
-
-request(urlReq, (error, response, body) => {
-  if (error) throw error;
-  response = JSON.parse(body);
-  const characters = response.characters;
-  //   console.log(response);
-  characters.forEach((value, ind) => {
-    // console.log(value);
-    request(value, (error, response, body) => {
-      if (error) throw error;
-      const char = JSON.parse(body);
-      console.log(char.name);
-    });
-  });
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
